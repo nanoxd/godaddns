bd510b3c00c8af720f62fc0e56ebf2163d99d823
Fernando Paredes <nano@fdp.io>
2017-01-11 10:55:01 -0800

    Setup clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate clap;
+
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    App::new("godaddns")
+        .version("1.0.0")
+        .author("Fernando Paredes <nano@fdp.io>")
+        .about("Update GoDaddy records to current IP")
+        .arg(Arg::with_name("DOMAIN")
+                .short("d")
+                .long("domain")
+                .required(true)
+                .help("Domain to update"))
+        .arg(Arg::with_name("RECORD_TYPE")
+                .required(true)
+                .short("r")
+                .long("record")
+                .help("Record type to set entry to"))
+        .get_matches();
+
 }
